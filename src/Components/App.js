3a61694dfb68590f63564ab36a7a139115ec0217
justingarcianm <justingarcianm@gmail.com>
2019-04-27 10:00:20 -0600

    add Navigation Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavWrapper from './Nav/NavWrapper'
 import '../styles/App.css';
 
 const App = () => {
   return (
     <>
       <h1>Hello!</h1>
+      <NavWrapper />
     </>
   )
 }
